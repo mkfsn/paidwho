b2f68e264a7c3ac102881243158cff5f8886c80b
mkfsn <p408865@gmail.com>
2017-09-08 16:55:51 +0800

    Remove unused sheet

--- console/src/services/sheet-data.ts
+++ console/src/services/sheet-data.ts
@@ -10,21 +10,7 @@ import { Record } from '../model/record';
 @Injectable()
 export class SheetData {
 
-    private sheet: Sheet;
-
     constructor(private storage: AsyncLocalStorage) {
-        this.sheet = new Sheet('Travel to Hokkaido!');
-
-        let foo: Person = new Person('foo');
-        let bar: Person = new Person('bar');
-
-        this.sheet.addMember(foo);
-        this.sheet.addMember(bar);
-
-        let thatday: Date = new Date('2017-09-01');
-        let lunch: Record = new Record('lunch', thatday, 1000, foo);
-
-        this.sheet.addRecord(lunch);
     }
 
     public get(id: string): Observable<any> {
@@ -34,6 +20,5 @@ export class SheetData {
     public set(sheet: Sheet): Observable<any> {
         return this.storage.setItem(sheet.id, sheet);
     }
-
 
 }
