87a46c99a73038651f463cddd890ed56697c2fa0
mkfsn <p408865@gmail.com>
2017-09-17 20:57:28 +0800

    Add db entry to store list of sheets

--- console/src/services/sheet-data.ts
+++ console/src/services/sheet-data.ts
@@ -7,10 +7,40 @@ import { Sheet } from '../model/sheet';
 import { Person } from '../model/person';
 import { Record } from '../model/record';
 
+interface ISheet {
+    id: string;
+    name: string;
+}
+
 @Injectable()
 export class SheetData {
 
+    private sheetsDBName = 'sheet-list';
+    private sheets: Array<ISheet>;
+
     constructor(private storage: AsyncLocalStorage) {
+        this.sheets = [];
+        this.load();
+    }
+
+    private load() {
+        this.storage.getItem(this.sheetsDBName).map((data) => {
+            return Array(data).map((v) => {
+                if (v === null) {
+                    return v;
+                }
+                return {id: v['id'], name: v['name']};
+            }).filter(v => v);
+        }).subscribe((sheets: Array<ISheet>) => {
+            this.sheets = sheets;
+        });
+    }
+
+    private save() {
+        this.storage.setItem(this.sheetsDBName, this.sheets).subscribe(
+            () => {},
+            () => {}
+        );
     }
 
     public get(id: string): Observable<Sheet> {
@@ -20,7 +50,18 @@ export class SheetData {
     }
 
     public set(sheet: Sheet): Observable<any> {
+        let index = this.sheets.findIndex((v) => {
+            return v.id === sheet.id;
+        });
+        if (index === -1) {
+            this.sheets.push({id: sheet.id, name: sheet.name});
+            this.save();
+        }
         return this.storage.setItem(sheet.id, sheet);
+    }
+
+    public all(): Array<ISheet> {
+        return this.sheets;
     }
 
 }
