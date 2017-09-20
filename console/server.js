e9bcab8ff59be8129d180504105d88a09ef22a37
mkfsn <p408865@gmail.com>
2017-09-20 21:40:47 +0800

    Add docker file for running APP

---
+++ console/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var httpProxy = require('http-proxy');
+
+var app = express();
+
+var path = __dirname;
+var port = 7001;
+
+var proxy = httpProxy.createProxyServer();
+
+function requestHandler(req, res) {
+    res.sendFile(path + '/index.html');
+}
+
+function requestForbidden(req, res) {
+    res.send(403);
+}
+
+app.use('/dist', express.static(path + '/dist'));
+
+app.get('/', requestHandler);
+app.get('/sheet', requestHandler);
+
+app.get('/src/*', requestForbidden);
+app.get('/node_modules/*', requestForbidden);
+
+app.listen(port);
